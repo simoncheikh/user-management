26624e3be2c85c0b42b343e253b4f6f0486a95c1
simoncheikh <simoncheikh710@gmail.com>
2025-04-14 16:32:10 +0300

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { viteMockServe } from 'vite-plugin-mock'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths(),
     viteMockServe({
       mockPath: 'src/mock',
       enable: true,
